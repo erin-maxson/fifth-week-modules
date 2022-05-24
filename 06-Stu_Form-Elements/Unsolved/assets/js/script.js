a85c025eb5140ba3ef31d4b03b7c20a128745706
erin-maxson <erin@cohatch.com>
2022-05-23 18:16:50 -0700

    activity changes

--- 06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,27 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
+var itemEl = $('#shopping-input');
+var toDoList = [];
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    console.log('Shopping List Item:', itemEl);
+
+    var listItem = itemEl.val();
+
+    toDoList.push(listItem);
+    display();
+}
+
+function display(){
+    shoppingListEl.empty();
+    for (let i = 0; i < toDoList.length; i++) {
+        shoppingListEl.append("<li>" + toDoList[i] + "</li>");
+    }
+}
+
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit', handleFormSubmit);
