d9dd416608ddd3e2c9f03c846407a09e4b71d73c
erin-maxson <erin@cohatch.com>
2022-05-25 16:04:14 -0700

    last week update

--- 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
+++ 08-Stu_Dom-Traversal/Unsolved/assets/js/script.js
@@ -2,5 +2,7 @@
 var rootEl = $('#root');
 
 // TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+rootEl.children("li").addClass("bg-light");
 
 // TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+rootEl.children().eq(3).children.eq(0).text("X");
